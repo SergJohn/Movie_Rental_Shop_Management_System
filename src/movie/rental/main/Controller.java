862f3bd531675be297b1724753125acca6e89104
mario rosa <mariosergiodublin@gmail.com>
2020-03-24 13:21:50 +0000

    Initial commit

---
+++ src/movie/rental/main/Controller.java
@@ -0,0 +1,22 @@
+package movie.rental.main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+*
+* @author mrosa
+*/
+public class Controller implements ActionListener {
+	
+	public Controller() {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/movie/rental/main/Main.java
@@ -0,0 +1,14 @@
+package movie.rental.main;
+
+/**
+*
+* @author mrosa
+*/
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Controller();
+	}
+
+}
